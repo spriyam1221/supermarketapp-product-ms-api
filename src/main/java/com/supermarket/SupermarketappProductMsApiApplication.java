64608dc24e9c1e6bccbabaca58540f1c92894853
spriyam1221 <spriyam1221@gmail.com>
2022-05-06 11:17:30 +0530

    first commit

---
+++ src/main/java/com/supermarket/SupermarketappProductMsApiApplication.java
@@ -0,0 +1,14 @@
+package com.supermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketappProductMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketappProductMsApiApplication.class, args);
+		System.out.println("hai");
+	}
+
+}
